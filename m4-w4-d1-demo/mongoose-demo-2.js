734030ac6f7b01252f2fe5592eb5ca226ce62442
Fan <52841919+fantastic-y@users.noreply.github.com>
2021-10-18 18:39:12 -0700

    finished

--- m4-w4-d1-demo/mongoose-demo-2.js
+++ m4-w4-d1-demo/mongoose-demo-2.js
@@ -6,5 +6,18 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   // we're connected!
 });
+const kittySchema = new mongoose.Schema({
+  name: String
+})
 
+kittySchema.methods.speak = function () {
+  const greeting = this.name
+    ? "Meow name is " + this.name
+    : "I don't have a name";
+  console.log(greeting);
+}
 
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+const fluffy = new Kitten({ name: 'fluffy' });
+fluffy.speak(); //"meow name is fluffy"
